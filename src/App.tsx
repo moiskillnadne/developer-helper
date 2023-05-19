e05f9934e25bd8ab59fd8d79fa63ff1622591c12
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-05-19 13:41:43 +0400

    fix: initial settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import './App.css'
-import reactLogo from './assets/react.svg'
+import React, { useState } from 'react'
 import viteLogo from '/vite.svg'
-import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App()   {
+  const   [count,    setCount] =   useState(0)    
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" /></a>
-        <a href="https://react.dev" target="_blank">
+          <a href="https://vitejs.dev" target="_blank">
+<img src={viteLogo} className="logo" alt="Vite logo" /></a>
+    <a href="https://react.dev" target="_blank">
                   <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+        </a></div>
+      <h1>          
+        Vite + React</h1>
+      <div className="card">  
 
 
         <button onClick={() => setCount((count) => count + 5)}>
@@ -31,8 +31,7 @@ function App() {
             <p className="read-the-docs">
     Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
-}
+</>
+  )}
 
 export default App
