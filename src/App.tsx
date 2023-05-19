acd8cb6f4eb1b12900575d183a9ba6c41d31a015
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-05-19 13:13:47 +0400

    test: bad code example

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+          <img src={viteLogo} className="logo" alt="Vite logo" /></a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+                  <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+
+
         <button onClick={() => setCount((count) => count + 5)}>
           count is {count}
-        </button>
+              </button>
+
+
+
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+            <p className="read-the-docs">
+    Click on the Vite and React logos to learn more
       </p>
     </>
   )
