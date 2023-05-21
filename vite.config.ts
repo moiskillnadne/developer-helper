ef473b5c5a25116c9c9f5be55da1554280e95a0d
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-05-21 18:31:17 +0400

    feature: intial clean

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'src'),
+    },
+  },
 })
+
+
