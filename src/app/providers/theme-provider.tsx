d40dd01f6a3133baf0dc5046c4202296553aee98
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-06-04 18:44:27 +0400

    feature: new palette

--- src/app/providers/theme-provider.tsx
+++ src/app/providers/theme-provider.tsx
@@ -6,14 +6,24 @@ type Props = PropsWithChildren<unknown>
 
 const theme = createTheme({
   palette: {
+    mode: "light",
     primary: {
-      main: "#8EEA6E",
+      main: "#75d054",
     },
     secondary: {
-      main: "#faedfa",
+      main: "#9400f5",
+    },
+    error: {
+      main: "#cb180a",
+    },
+    success: {
+      main: "#1e8123",
+    },
+    warning: {
+      main: "#f5a623",
     },
     info: {
-      main: "#30b237",
+      main: "#2196f3",
     },
   },
 })
