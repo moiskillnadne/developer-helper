f09edfb475e89b34e7b78fab0a0787552f773c26
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-06-06 11:01:07 +0400

    feature: login form valdiation

--- src/shared/ui/TextInputBase/index.tsx
+++ src/shared/ui/TextInputBase/index.tsx
@@ -1,4 +1,4 @@
-import { FormControl, InputLabel, OutlinedInput, TextField } from "@mui/material"
+import { FormControl, FormHelperText, InputLabel, OutlinedInput, TextField } from "@mui/material"
 import { useController, useFormContext } from "react-hook-form"
 
 type Props = React.ComponentProps<typeof TextField> & {
@@ -11,6 +11,7 @@ export const TextInputBase = (props: Props) => {
   const { control } = useFormContext()
   const {
     field: { onChange: onFormChange },
+    fieldState: { error, invalid },
   } = useController({ name, control })
 
   const onChangeHanlder = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,9 +25,10 @@ export const TextInputBase = (props: Props) => {
   }
 
   return (
-    <FormControl sx={sx}>
-      <InputLabel htmlFor="component-outlined">{name}</InputLabel>
-      <OutlinedInput id="component-outlined" label={name} onChange={onChangeHanlder} />
+    <FormControl sx={sx} error={invalid}>
+      <InputLabel htmlFor="base-text-input">{name}</InputLabel>
+      <OutlinedInput id="base-text-input" label={name} onChange={onChangeHanlder} />
+      <FormHelperText id="base-text-input">{error?.message}</FormHelperText>
     </FormControl>
   )
 }
