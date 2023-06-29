889e870ccb02eb8add3c48e2037d28a5c54189c2
Riabkov, Viktor <vriabkov@scnsoft.com>
2023-06-29 22:05:52 +0400

    fix: new fonts

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,7 +4,7 @@ import CssBaseline from "@mui/material/CssBaseline"
 import ReactDOM from "react-dom/client"
 
 import App from "./App.tsx"
-import "../assets/fonts/Nunito/nunito.style.css"
+import "../assets/fonts/Comfortaa/comfortaa.styles.css"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
